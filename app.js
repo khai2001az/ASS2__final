34b5ac1c56f30d79e4d5537ed51c5eefc54bc710
Khai2001az <01658904266az@gmail.com>
2021-10-26 14:46:53 +0700

    dev

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(session({
     cookie: { maxAge: 600000 }
 }));
 
+
 app.get('/edit', async(req, res) => {
     const id = req.query.id;
 
@@ -38,6 +39,7 @@ app.post('/update', async(req, res) => {
     const pictureInput = req.body.txtPicture;
     const id = req.body.txtId;
 
+    
     updateProduct(id, nameInput, priceInput, pictureInput);
     res.redirect("/");
 })
@@ -48,34 +50,22 @@ app.post('/insert', async(req, res) => {
     const nameInput = req.body.txtName;
     const priceInput = req.body.txtPrice;
     const pictureInput = req.body.txtPicture;
-
-   const upfirst =  nameInput.charAt(0).toUpperCase() + nameInput.slice(1)
-   console.log(upfirst);
-
     const newProduct = {name: nameInput, price: priceInput, imgUrl: pictureInput, size: { dai: 20, rong: 40 } }
     
-    if (nameInput.length <= 2) {
-        res.render("insert", { errorMsg: 'Name should be more than 2 character' })
+    if (nameInput.length  < 4) {
+        res.render("insert", { errorMsg: 'Name should be more than 4 character' })
         return;
-    }
-    
+    } 
     await insertProduct(newProduct, "SanPham");
     res.redirect("/");
 })
 app.get('/delete', async(req, res) => {
     const id = req.query.id;
 
+    
     await deleteProduct(id);
     res.redirect("/");
 })
-
-// app.post('/search', async(req, res) => {
-//     const searchInput = req.body.txtSearch;
-//     const dbo = await getDB()
-//     const allProducts = await dbo.collection("SanPham").find({ name: searchInput }).toArray();
-
-//     res.render('index', { data: allProducts })
-// })
 
 app.get('/', async(req, res) => {
     const dbo = await getDB();
